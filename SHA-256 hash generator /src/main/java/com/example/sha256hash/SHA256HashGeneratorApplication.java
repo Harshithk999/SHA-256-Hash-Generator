21c45542dff2bbe7c74cb2f0328b7238b6d6d577
Harshith K <harshithkotian999@gmail.com>
2024-06-09 11:04:46 +0530

    Add files via upload

---
+++ SHA-256 hash generator /src/main/java/com/example/sha256hash/SHA256HashGeneratorApplication.java
@@ -0,0 +1,11 @@
+package com.example.sha256hash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SHA256HashGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SHA256HashGeneratorApplication.class, args);
+    }
+}
